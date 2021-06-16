1251db599af9663760c054b37fbcbfaefc15db82
Vasile Morari <VMorar@alfabank.ru>
2021-06-16 13:52:29 +0300

    ParallaxView moved to other file

--- parallax_scrollView/ScrollViews/ParallaxScrollView.swift
+++ parallax_scrollView/ScrollViews/ParallaxScrollView.swift
@@ -31,3 +31,46 @@ class ParallaxScrollView<T: UIView>: InfiniteScrollView<ParallaxView<T>> {
         }
     }
 }
+
+class ParallaxView<T: UIView>: UIView {
+    
+    // MARK: - Content
+    
+    let contentView: T
+    
+    init(contentView: T) {
+        self.contentView = contentView
+        super.init(frame: contentView.frame)
+        
+        layer.masksToBounds = true
+        
+        addContentView(contentView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private
+    
+    private func addContentView(_ contentView: T) {
+        addSubview(contentView)
+        
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            contentView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
+// MARK: - Instantiatable
+
+extension ParallaxView: Instantiatable {
+    static func instantiate() -> Self {
+        return ParallaxView(contentView: T()) as! Self
+    }
+}
