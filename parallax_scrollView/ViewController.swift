e32774bfcbd14966f103c96374f2de64715410cb
Vasile Morari <VMorar@alfabank.ru>
2021-06-16 13:15:05 +0300

    Created template iOS project

---
+++ parallax_scrollView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parallax_scrollView
+//
+//  Created by Vasile Morari on 16.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
